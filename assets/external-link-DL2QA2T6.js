75bba07b85a226bc8adabc2a09daa75ef8913f1b
ebonyschneider462359 <anderson.conger9@gmail.com>
2025-06-01 18:36:23 -0400

    More default addresses

--- assets/external-link-DL2QA2T6.js
+++
@@ -1,8 +0,0 @@
-import{b as l}from"./core-QEv20hSy.js";import"./index-CZz4su9W.js";import"./Web3Context-CGqy749v.js";import"./index.es-3g1222S5.js";const t=l`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as externalLinkSvg};
